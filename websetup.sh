2fa23cd6f6f98e9fd8775535188270a2e9932e52
J-e-i-m-z <nyamburijames@gmail.com>
2024-09-05 20:30:03 +0000

    Initial Commit

---
+++ websetup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Installing Dependancies
+echo "##############################################"
+echo "Installing Packages"
+echo "##############################################"
+sudo yum install wget unzip httpd -y > /dev/null
+echo
+
+#Start & Enable Service
+echo "##############################################"
+echo "Start and Enable httpd Service"
+echo "##############################################"
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo
+
+# Creating Temp Directory
+echo "##############################################"
+echo "Starting Artifact Deployment"
+echo "##############################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
+unzip 2098_health.zip > /dev/null
+sudo cp -r 2098_health/* /var/www/html/
+echo
+
+# Bounce Service
+echo "##############################################"
+echo "Restarting HTTPD Service"
+echo "##############################################"
+sudo systemctl restart httpd 
+echo
+
+rm -rf /tmp/webfiles
+echo
+
+sudo systemctl status httpd
+ls /var/www/html/
+echo
+
+
